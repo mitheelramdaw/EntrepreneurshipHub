9f957aecf75a7e3ed01d58458389664c9e22f570
mitheelramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2022-08-04 12:32:16 +0200

    Upload Website Code

---
+++ javascript/anything.js
@@ -0,0 +1,12 @@
+let input = prompt("Enter age");
+
+switch (input) {
+    case "18":
+        alert("You are off age!");
+        break;
+    case "12":
+        alert("You are underage");
+        break;
+    default:
+        alert("Hi");
+}
